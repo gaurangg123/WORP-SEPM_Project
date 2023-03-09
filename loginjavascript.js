e90e36acc20b5c240b6870a6305a750698ca09e7
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-03-09 18:11:49 +0530

    Add files via upload

---
+++ loginjavascript.js
@@ -0,0 +1,5 @@
+function showPassword() {
+    var passwordInput = document.getElementById("password");
+    var showPassword = document.querySelector(".show-password");
+    if (passwordInput.type === "password") {
+       
