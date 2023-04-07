7de7b03efd6e0610aa6df74fa31837897e682a16
jamogh <jamogh83@gmail.com>
2023-04-07 11:15:51 +0530

    NEW

--- loginjavascript.js
+++ loginjavascript.js
@@ -4,3 +4,7 @@ function showPassword() {
     if (passwordInput.type === "password") {
     }
 }
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
