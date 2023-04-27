372e40065dd25d344337ffdb23c6384c35580fd6
Gaurang Ashava <gaurangashava@gmail.com>
2023-04-27 19:05:39 +0530

    update
    update

--- registerdb.php
+++ registerdb.php
@@ -3,30 +3,35 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "registration";
+$dbname = "login";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Prepare statement
-$stmt = mysqli_prepare($conn, "INSERT INTO users (username, firstname, lastname, password, birthdate, email) VALUES (?, ?, ?, ?, ?, ?)");
 
-
-mysqli_stmt_bind_param($stmt, "ssssss", $username, $firstname, $lastname, $password, $birthdate, $email);
-
-// Set parameters
 $username = $_POST['username'];
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$birthdate = $_POST['birthdate'];
+$password = $_POST['password'];
+$birthdate = $_POST['dob'];
+$phone = $_POST['phone'];
 $email = $_POST['email'];
+// Prepare statement
+$stmt = mysqli_prepare($conn, "INSERT INTO users (username, firstname, lastname, password, birthdate, phone_no, email) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+
+mysqli_stmt_bind_param($stmt, "sssssds", $username, $firstname, $lastname, $password, $birthdate, $phone, $email);
+
+// Set parameters
 
 // Execute statement
 mysqli_stmt_execute($stmt);
+
 
 echo "Registration successful.";
 
